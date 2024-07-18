9e1ab3cdf80210f95a6ed80ed122e898c4b616c9
“liljet” <liljet@ingru.ai>
2024-07-18 15:03:11 +0800

    '完善新闻模版'

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -3,28 +3,17 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Descriptions, message } from 'antd';
 import axiosReq from '@/utils/req'
 import { useStore } from "@/store/useStore";
-import HomePage from "@/components/HomeComponent/HomePage";
-import FastExperience from "@/components/HomeComponent/FastExperience";
-import Overview from "@/components/HomeComponent/Overview";
+
 import bg_png from '@/assets/img/bgImg.png';
-import star_png from '@/assets/img/star.png';
-import text_png from '@/assets/img/text.png';
-import arrows_png from '@/assets/img/arrows.png';
-import camera_png from '@/assets/img/camera.png';
+
 import safe_png from '@/assets/img/safe.png';
-import shandian_png from '@/assets/img/shandian.png';
+
 import agentNet_png from '@/assets/img/AgentNet.png';
 import LMCloud_png from '@/assets/img/LMCloud.png';
 import icon11_png from '@/assets/img/icon11.png';
 import icon22_png from '@/assets/img/icon22.png';
 import icon33_png from '@/assets/img/icon33.png';
-import vx_svg from '@/assets/svg/vx.svg';
-import zh_svg from '@/assets/svg/zh.svg';
-import bg2_png from '@/assets/img/bg2.png';
-import star2_png from '@/assets/img/star2.png';
-import beacon_png from '@/assets/img/beacon.png';
-import suanli1_png from '@/assets/img/suanli1.png';
-import suanli2_png from '@/assets/img/suanli2.png';
+
 
 import news1_png from '@/assets/img/news1.png'
 import news2_png from '@/assets/img/news2.png'
@@ -94,22 +83,29 @@ const News: React.FC = () => {
         { name: '合作生态', key: 'cooperationEcology' }
     ];
 
-    const newsData: any = [{ type: 'businessMovement', imageSrc: news1_png, icon: icon33_png, title: '隐入（浙江）科技有限公司应邀参加“环浙工大创新经济圈“ 创业兴业恳谈会成功签...', description: '隐入（浙江）科技有限公司于7月4日应邀参加了“康”企乐业“乾”图大展“环浙工大创新经济圈”创业兴业恳谈会暨半年度“人工智能+”产业链项目集中签约活动，并在此次活动中成功签约重要合作项目。彰显了公司在推动人工智能应用和行业发展方面的地位及未来发展愿景。' },
-    { type: 'technologyMovement', imageSrc: news2_png, icon: icon11_png, title: '爆火的Sora，对医疗领域会有哪些影响？', description: '北京时间2月16日凌晨，OpenAI宣布了一项重大突破：其全新研发的文生视频大模型Sora正式亮相。继文字、图片之后，全世界再一次被OpenAI震撼了。几乎没有AI制作痕迹的流畅视频，让社交媒体惊呼：现实，是不是不存在了？' },
-    { type: 'industryMovement', imageSrc: news3_png, icon: icon22_png, title: '孙凝晖院士给正国级、副国级讲课的万字长稿发布——《人工智能与智能计算的发展》', description: '中国人大网近日刊登孙凝晖在十四届全国人大常委会专题讲座上的讲稿《人工智能与智能计算的发展》，现将全文转载如下，让我们一同走进高深莫测的人工智能世界。' }
+    const newsData: any = [{ type: 'businessMovement', id: 1, imageSrc: news1_png, icon: icon33_png, title: '隐入（浙江）科技有限公司应邀参加“环浙工大创新经济圈“ 创业兴业恳谈会成功签...', description: '隐入（浙江）科技有限公司于7月4日应邀参加了“康”企乐业“乾”图大展“环浙工大创新经济圈”创业兴业恳谈会暨半年度“人工智能+”产业链项目集中签约活动，并在此次活动中成功签约重要合作项目。彰显了公司在推动人工智能应用和行业发展方面的地位及未来发展愿景。' },
+    { type: 'technologyMovement', id: 2, imageSrc: news2_png, icon: icon11_png, title: '爆火的Sora，对医疗领域会有哪些影响？', description: '北京时间2月16日凌晨，OpenAI宣布了一项重大突破：其全新研发的文生视频大模型Sora正式亮相。继文字、图片之后，全世界再一次被OpenAI震撼了。几乎没有AI制作痕迹的流畅视频，让社交媒体惊呼：现实，是不是不存在了？' },
+    { type: 'industryMovement', id: 3, imageSrc: news3_png, icon: icon22_png, title: '孙凝晖院士给正国级、副国级讲课的万字长稿发布——《人工智能与智能计算的发展》', description: '中国人大网近日刊登孙凝晖在十四届全国人大常委会专题讲座上的讲稿《人工智能与智能计算的发展》，现将全文转载如下，让我们一同走进高深莫测的人工智能世界。' }
     ]
 
-    const [newsDatalist, setNewsData] = useState([])
+    const [newsDatalist, setNewsData] = useState(newsData)
+
+
 
     const changeType = (item) => {
         setCurrentType(item.key)
         console.log(item)
-        const newsArr = newsData.filter((itemnews, index) => {
+        if (item.key == 'all') {
+            setNewsData(newsData)
+        } else {
+            const newsArr = newsData.filter((itemnews, index) => {
+                return item.key == itemnews.type
+            })
+            setNewsData(newsArr)
+        }
 
-            // console.log(item.type, itemnews.key, '111')
-            return item.key == itemnews.type
-        })
-        console.log(newsArr, 'aaaaaa')
+
+
     }
 
 
@@ -117,9 +113,9 @@ const News: React.FC = () => {
     // px-3.5 lg:py-2.5 rounded-lg border border-solid border-slate-800
     const TabMenu = () => {
         return (
-            <div style={{ fontFamily: 'alibb-light' }} className=" text-[12px] lg:text-16px flex gap-5 items-center  text-violet-100 text-opacity-70 ">
+            <div style={{ fontFamily: 'alibb-light' }} className=" text-[12px] lg:text-16px flex  gap-3 lg:gap-5 items-center  text-violet-100 text-opacity-70 ">
                 {typedata.map((item, index) => {
-                    return <div onClick={() => changeType(item)} key={index} className={`${item.key == currentType ? 'rounded-lg border border-solid border-slate-800' : 'border border-transparent'} px-3.5 lg:py-2.5  self-stretch my-auto cursor-pointer z-10`}>{item.name}</div>
+                    return <div onClick={() => changeType(item)} key={index} className={`${item.key == currentType ? 'rounded-lg border border-solid border-slate-800' : 'border border-transparent'} px-2  lg:px-3.5 lg:py-2.5  self-stretch my-auto cursor-pointer z-10`}>{item.name}</div>
                 })}
                 {/* <div className="self-stretch my-auto cursor-pointer z-10 ">企业动态</div>
                 <div className="self-stretch my-auto cursor-pointer z-10" >行业动态</div>
@@ -132,15 +128,16 @@ const News: React.FC = () => {
 
 
     const router = useRouter()
-    const toNewsContent = (id) => {
-        router.push(`/news/newsdetail/${id + 1}/`)
+    const toNewsContent = (item) => {
+
+        router.push(`/news/newsdetail/${item.id}/`)
     }
 
     const NewsContent = ({ index, item }) => {
         return (
             <>
                 <div className="mt-16 w-full ">
-                    <div className="lg:flex   gap-8 cursor-pointer z-200  relative " onClick={() => toNewsContent(index)}>
+                    <div className="lg:flex   gap-8 cursor-pointer z-200  relative " onClick={() => toNewsContent(item)}>
                         <Image src={item.imageSrc} className="w-3/4 h-[120px] mx-auto  lg:w-[320px]  bg-gray-800 rounded-xl lg:h-[168px]" alt="bg" />
                         <div className="flex flex-col max-w-[762px] ">
                             <div className="  mt-10 lg:mt-0 flex flex-col self-stretch px-5  text-base font-light text-violet-100 text-opacity-70  ">
@@ -171,11 +168,10 @@ const News: React.FC = () => {
                     <div className=" mt-6 text-titleColor px-15 lg:px-0    text-center  lg:w-[480px] lg:leading-6 text-[12px]  lg:text-16px" style={{ color: '#B4BCD0', fontFamily: 'alibb-light' }}  >最全面及时的行业资讯、企业动态</div>
                 </div>
                 <div className=" mt-10 lg:mt-34  w-full" style={{ height: '1px', background: ' linear-gradient(90deg, rgba(42, 45, 68, 0.00) 0%, #2A2D44 50%, rgba(42, 45, 68, 0.00) 100%)' }}></div>
-
                 <div className=" mt-[60px] w-full px-[10px] lg:px-[164px]">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col  min-h-[200px] lg:min-h-[500px]">
                         <TabMenu></TabMenu>
-                        {newsData.map((item, index) => {
+                        {newsDatalist.map((item, index) => {
                             return (
                                 <NewsContent item={item} key={index} index={index} />
                             )
--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -25,14 +25,14 @@ const News: React.FC = ({ id }) => {
             setNewsDetail(newsId1)
             setTitle({ next: '孙凝晖院士给正国级、副国级讲课的万字长稿发布——《人工智能与智能计算的发展》', })
         }
-        if (id == 2) {
-            setNewsDetail(newsId2)
-            setTitle({ next: '爆火的Sora，对医疗领域会有哪些影响？', last: '隐入（浙江）科技有限公司应邀参加“环浙工大创新经济圈“ 创业兴业恳谈会成功签约项目' })
-        }
-        if (id == 3) {
-            setNewsDetail(newsId3)
-            setTitle({ last: '孙凝晖院士给正国级、副国级讲课的万字长稿发布——《人工智能与智能计算的发展》', })
-        }
+        // if (id == 2) {
+        //     setNewsDetail(newsId2)
+        //     setTitle({ next: '爆火的Sora，对医疗领域会有哪些影响？', last: '隐入（浙江）科技有限公司应邀参加“环浙工大创新经济圈“ 创业兴业恳谈会成功签约项目' })
+        // }
+        // if (id == 3) {
+        //     setNewsDetail(newsId3)
+        //     setTitle({ last: '孙凝晖院士给正国级、副国级讲课的万字长稿发布——《人工智能与智能计算的发展》', })
+        // }
 
     }, [])
 
@@ -57,10 +57,15 @@ const News: React.FC = ({ id }) => {
 
 
     return (
+        // <div className="w-full max-w-[1440px]  items-center  px-[20px] lg:px-[152px]">
+        //     <div dangerouslySetInnerHTML={{ __html: newsdetail }} className="rich-text  mt-20 " />
+
+        // </div>
+
         <HeaderLayout>
             <div className=" ">
                 <div className="w-full max-w-[1440px]  items-center  px-[20px] lg:px-[152px]">
-
+                    {/* <iframe src="https://a.xiumi.us/board/v5/4bkfR/552832590" className="mt-20" style={{ width: '100%', height: '2400px' }} ></iframe> */}
                     <div dangerouslySetInnerHTML={{ __html: newsdetail }} className="rich-text  mt-20 " />
                     <div className="shrink-0 mt-11 h-px border border-solid bg-slate-800 border-slate-800 max-md:mt-10 max-md:max-w-full" />
                     <div className="flex gap-5 px-px mt-11 ">
